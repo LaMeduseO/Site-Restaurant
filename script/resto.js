326358c70157312ad659dc4db3ed9cfaa2b67c2d
LaMeduseO <vincepro59@gmail.com>
2025-06-18 11:50:08 +0200

    Nav Button Underline ok

---
+++ script/resto.js
@@ -0,0 +1,21 @@
+let underline = document.querySelectorAll('.underline')
+let triggerBox = document.querySelectorAll('.button-box')
+
+function transition(currentLine) {
+
+    currentLine.style.width = '100%'
+}
+
+function transitionBack(currentLine) {
+    setTimeout(()=>{currentLine.style.width = '0%'}, 400)
+}
+
+
+for(let i = 0; i<triggerBox.length;i++) {
+    triggerBox[i].addEventListener("mouseover", transition.bind(null, underline[i]))
+    triggerBox[i].addEventListener("mouseleave", transitionBack.bind(null, underline[i]))
+}
+
+/*triggerBox.addEventListener("mouseover", transition)
+triggerBox.addEventListener("mouseleave", transitionBack)*/
+

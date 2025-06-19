5860c94ef8844fc4e89503ef2ba37339566851b2
LaMeduseO <vincepro59@gmail.com>
2025-06-19 11:47:53 +0200

    Nav Bar Ok Transition ok

--- script/resto.js
+++ script/resto.js
@@ -1,5 +1,7 @@
 let underline = document.querySelectorAll('.underline')
 let triggerBox = document.querySelectorAll('.button-box')
+let nav = document.querySelector('nav')
+let button = document.querySelectorAll('.nav-button')
 
 function transition(currentLine) {
 
@@ -15,6 +17,17 @@ for(let i = 0; i<triggerBox.length;i++) {
     triggerBox[i].addEventListener("mouseover", transition.bind(null, underline[i]))
     triggerBox[i].addEventListener("mouseleave", transitionBack.bind(null, underline[i]))
 }
+
+function ChangeTextColor(){
+    button.forEach((e)=>{e.style.color="var(--couleur-Smoky-Brown)"})
+}
+
+function BackTextColor(){
+    button.forEach((e)=>{e.style.color="var(--couleur-Creamy-Beige)"})
+}
+
+nav.addEventListener("mouseover", ChangeTextColor)
+nav.addEventListener("mouseleave", BackTextColor)
 
 /*triggerBox.addEventListener("mouseover", transition)
 triggerBox.addEventListener("mouseleave", transitionBack)*/
